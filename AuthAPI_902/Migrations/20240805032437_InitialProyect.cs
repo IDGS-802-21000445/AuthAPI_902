44620a2a26f369b2b4a6a71f13e6883ca568d83f
EdwinY-a <21000445@alumnos.utleon.edu.mx>
2024-08-05 23:25:55 -0600

    Agregar archivos de proyecto.

---
+++ AuthAPI_902/Migrations/20240805032437_InitialProyect.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AuthAPI_902.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialProyect : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
